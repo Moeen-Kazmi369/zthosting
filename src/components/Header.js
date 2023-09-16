85c5d09976597b7ea2947252a4d9c1bc02d4b1ab
@codewithkazmi <115881946+Moeen-Kazmi369@users.noreply.github.com>
2023-09-16 05:12:27 -0700

    jkseh

--- src/components/Header.js
+++ src/components/Header.js
@@ -237,7 +237,7 @@ export default function Example() {
         onClose={setMobileMenuOpen}
       >
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className={`fixed inset-y-0 right-0 z-10 w-full overflow-y-auto ${theme?"bg-white":"bg-black text-white"} px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10`}>
+        <Dialog.Panel className={`fixed inset-y-0 right-0 z-10 w-full overflow-y-auto ${theme?"bg-black text-white":"bg-white"} px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10`}>
           <div className="flex items-center justify-between">
             <Link href={"/"}>
               <figure className="footer_logo mb-0">
