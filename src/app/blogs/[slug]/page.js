66fa48bbdd5ebab2afe7d97d5d6a6857a7a967a6
@codewithkazmi <115881946+Moeen-Kazmi369@users.noreply.github.com>
2023-09-22 04:02:04 -0700

    futguytgfyu

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -15,56 +15,62 @@ const Page = () => {
     };
     fetchData(slug);
   }, []);
+  console.log(Data);
   return (
     <>
       <div className="flex justify-center">
         <div className="flex flex-col justify-center">
           <div className="flex flex-col md:flex-row max-w-7xl justify-center items-center ">
             <div className="overflow-hidden w-full m-4 shadow-sm flex flex-col justify-center">
-              {Data && (
-                <div className="flex flex-col md:flex-row items-center">
-                <h1 clasName="text-black text-center text-xl font-semibold">{Data}</h1>
-                  <div className=" w-full overflow-hidden flex items-center justify-center">
-                    {" "}
-                    <img
-                      src={Data["_embedded"]["wp:featuredmedia"][0]["source_url"]}
-                      alt=""
-                      className="w-[45vw] h-[30vw]"
-                    />{" "}
-                  </div>
-                  <div className="md:w-2/3 m-4 ">
-                    <div className="flex text-gray-500 text-sm m-2">
-                      <div className="m-1 font-bold">Vlog,</div>
-                      <div className="m-1">31 March, 2023</div>
-                    </div>
-                    <div className="font-bold text-black text-xl m-2">
-                      {Data.title.rendered}
-                    </div>
-                    <div dangerouslySetInnerHTML={{__html:Data.excerpt.rendered}} >
-                    </div>
-                    <div className="flex cursor-pointer">
-                      <div className="m-2">
+              {Data && Data.map((e,i)=>{
+                console.log(e);
+                return(
+                    <>
+                    <div key={e.id} className="flex flex-col md:flex-row items-center">
+                    {/* <h1 clasName="text-black text-center text-xl font-semibold">{e}</h1> */}
+                      <div className=" w-full overflow-hidden flex items-center justify-center">
                         {" "}
                         <img
-                          src="https://source.unsplash.com/50x50/?man"
+                          src={e._embedded["wp:featuredmedia"][0].source_url}
                           alt=""
-                          className=" rounded-full"
+                          className="w-[45vw] h-[30vw]"
                         />{" "}
                       </div>
-                      <div className="grid m-1">
-                        <div className="font-bold text-sm hover:text-gray-600 mt-2">
-                          Jason Bourne
+                      <div className="md:w-2/3 m-4 ">
+                        <div className="flex text-gray-500 text-sm m-2">
+                          <div className="m-1 font-bold">Vlog,</div>
+                          <div className="m-1">31 March, 2023</div>
                         </div>
-                        <div className=" text-sm hover:text-gray-600">
-                          Student, Los Angeles
+                        <div className="font-bold text-black text-xl m-2">
+                          {e.title.rendered}
+                        </div>
+                        <div dangerouslySetInnerHTML={{__html:e.excerpt.rendered}} >
+                        </div>
+                        <div className="flex cursor-pointer">
+                          <div className="m-2">
+                            {" "}
+                            <img
+                              src="https://source.unsplash.com/50x50/?man"
+                              alt=""
+                              className=" rounded-full"
+                            />{" "}
+                          </div>
+                          <div className="grid m-1">
+                            <div className="font-bold text-sm hover:text-gray-600 mt-2">
+                              Jason Bourne
+                            </div>
+                            <div className=" text-sm hover:text-gray-600">
+                              Student, Los Angeles
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-              )}
-              <div className="px-16 py-10" dangerouslySetInnerHTML={{__html: Data && Data.content.rendered}}>
-              </div>
+                    <div className="px-16 py-10" dangerouslySetInnerHTML={{__html: e && e.content.rendered}}>
+                    </div>
+                    </>
+                )
+              })}
             </div>
           </div>
         </div>
