ce403e87b12d8b15c4ee89ec9cd8c91ef25bd59e
@codewithkazmi <115881946+Moeen-Kazmi369@users.noreply.github.com>
2023-09-22 00:01:47 -0700

    hbub

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,16 +1,16 @@
-import axios from "axios";
-async function fetchWhmcsData(path) {
-    const url = `http://my.zthosting.com/path/to/whmcs/includes/api.php`;
+// import axios from "axios";
+// async function fetchWhmcsData(path) {
+//     const url = `http://my.zthosting.com/path/to/whmcs/includes/api.php`;
   
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: `Bearer ${process.env.API_IDENTIFER}:${process.env.API_SECERT}`,
-      },
-    });
+//     const response = await axios.get(url, {
+//       headers: {
+//         Authorization: `Bearer ${process.env.API_IDENTIFER}:${process.env.API_SECERT}`,
+//       },
+//     });
   
-    return response.data;
-  }
-  export async function GET(request) {
-    const res=await fetchWhmcsData();
-    console.log(res);
-  }  
+//     return response.data;
+//   }
+//   // export async function GET(request) {
+//   //   const res=await fetchWhmcsData();
+//   //   console.log(res);
+//   // }  
