76d7f63ca01ed27c4fc82a940bc3127ec475fc26
@codewithkazmi <115881946+Moeen-Kazmi369@users.noreply.github.com>
2023-09-22 02:57:33 -0700

    bcjvwu

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -1,10 +1,10 @@
-// "use client";
-// import React from "react";
-// import { useParams } from "next/navigation";
-// import { useEffect, useState } from "react";
-// const Page = () => {
-//   const [data, setdata] = useState(null);
-//   const { slug } = useParams();
+"use client";
+import React from "react";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+const Page = () => {
+  const [data, setdata] = useState(null);
+  const { slug } = useParams();
 //   useEffect(() => {
 //     const fetchData = async () => {
 //       const res = await getServerSideProps(slug);
@@ -12,73 +12,74 @@
 //     };
 //     fetchData();
 //   }, []);
-// //   console.log(data);
-//   return (
-//     <>
-//       <div className="flex justify-center">
-//         <div className="flex flex-col justify-center">
-//           <div className="flex flex-col md:flex-row max-w-7xl justify-center items-center ">
-//             <div className="overflow-hidden w-full m-4 shadow-sm flex flex-col justify-center">
-//               {data && (
-//                 <div className="flex flex-col md:flex-row items-center">
-//                   <div className=" w-full overflow-hidden flex items-center justify-center">
-//                     {" "}
-//                     <img
-//                       src={data["_embedded"]["wp:featuredmedia"][0]["source_url"]}
-//                       alt=""
-//                       className="w-[45vw] h-[30vw]"
-//                     />{" "}
-//                   </div>
-//                   <div className="md:w-2/3 m-4 ">
-//                     <div className="flex text-gray-500 text-sm m-2">
-//                       <div className="m-1 font-bold">Vlog,</div>
-//                       <div className="m-1">31 March, 2023</div>
-//                     </div>
-//                     <div className="font-bold text-black text-xl m-2">
-//                       {data.title.rendered}
-//                     </div>
-//                     <div dangerouslySetInnerHTML={{__html:data.excerpt.rendered}} >
-//                     </div>
-//                     <div className="flex cursor-pointer">
-//                       <div className="m-2">
-//                         {" "}
-//                         <img
-//                           src="https://source.unsplash.com/50x50/?man"
-//                           alt=""
-//                           className=" rounded-full"
-//                         />{" "}
-//                       </div>
-//                       <div className="grid m-1">
-//                         <div className="font-bold text-sm hover:text-gray-600 mt-2">
-//                           Jason Bourne
-//                         </div>
-//                         <div className=" text-sm hover:text-gray-600">
-//                           Student, Los Angeles
-//                         </div>
-//                       </div>
-//                     </div>
-//                   </div>
-//                 </div>
-//               )}
-//               <div className="px-16 py-10" dangerouslySetInnerHTML={{__html: data && data.content.rendered}}>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       <script src="https://cdn.tailwindcss.com"></script>
-//       <script src="https://use.fontawesome.com/03f8a0ebd4.js"></script>
-//       <script
-//         type="module"
-//         src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
-//       ></script>
-//       <script
-//         nomodule
-//         src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
-//       ></script>
-//     </>
-//   );
-// };
+//   console.log(data);
+  return (
+    <>
+      <div className="flex justify-center">
+        <div className="flex flex-col justify-center">
+          <div className="flex flex-col md:flex-row max-w-7xl justify-center items-center ">
+            <div className="overflow-hidden w-full m-4 shadow-sm flex flex-col justify-center">
+                <h1 className="text-black text-center text-xl font-semibold">{slug}</h1>
+              {data && (
+                <div className="flex flex-col md:flex-row items-center">
+                  <div className=" w-full overflow-hidden flex items-center justify-center">
+                    {" "}
+                    <img
+                      src={data["_embedded"]["wp:featuredmedia"][0]["source_url"]}
+                      alt=""
+                      className="w-[45vw] h-[30vw]"
+                    />{" "}
+                  </div>
+                  <div className="md:w-2/3 m-4 ">
+                    <div className="flex text-gray-500 text-sm m-2">
+                      <div className="m-1 font-bold">Vlog,</div>
+                      <div className="m-1">31 March, 2023</div>
+                    </div>
+                    <div className="font-bold text-black text-xl m-2">
+                      {data.title.rendered}
+                    </div>
+                    <div dangerouslySetInnerHTML={{__html:data.excerpt.rendered}} >
+                    </div>
+                    <div className="flex cursor-pointer">
+                      <div className="m-2">
+                        {" "}
+                        <img
+                          src="https://source.unsplash.com/50x50/?man"
+                          alt=""
+                          className=" rounded-full"
+                        />{" "}
+                      </div>
+                      <div className="grid m-1">
+                        <div className="font-bold text-sm hover:text-gray-600 mt-2">
+                          Jason Bourne
+                        </div>
+                        <div className=" text-sm hover:text-gray-600">
+                          Student, Los Angeles
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+              <div className="px-16 py-10" dangerouslySetInnerHTML={{__html: data && data.content.rendered}}>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <script src="https://cdn.tailwindcss.com"></script>
+      <script src="https://use.fontawesome.com/03f8a0ebd4.js"></script>
+      <script
+        type="module"
+        src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+      ></script>
+      <script
+        nomodule
+        src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+      ></script>
+    </>
+  );
+};
 // export async function getServerSideProps(slug) {
 // //   console.log(slug);
 //   const res = await fetch(
@@ -89,13 +90,13 @@
 //   // Pass data to the page via props
 //   return { props: { data } };
 // }
-// export default Page;
-import React from 'react'
+export default Page;
+// import React from 'react'
 
-const page = () => {
-  return (
-    <div>Testing</div>
-  )
-}
+// const page = () => {
+//   return (
+//     <div>Testing</div>
+//   )
+// }
 
-export default page
+// export default page
