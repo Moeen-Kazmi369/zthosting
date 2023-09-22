13512f6ac8a7fe4e1153807e21fdc58c0c466169
@codewithkazmi <115881946+Moeen-Kazmi369@users.noreply.github.com>
2023-09-22 02:53:37 -0700

    jcnsn

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -90,3 +90,12 @@
 //   return { props: { data } };
 // }
 // export default Page;
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Testing</div>
+  )
+}
+
+export default page
