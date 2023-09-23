06dbe39a9b0b9b040a5252ff448e21dc805b693f
@codewithkazmi <115881946+Moeen-Kazmi369@users.noreply.github.com>
2023-09-23 05:33:48 -0700

    jwufygwuy

--- src/components/Home/Recently_Posts.js
+++ src/components/Home/Recently_Posts.js
@@ -1,130 +1,133 @@
 "use client";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import { useRef } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper/modules";
 const Recently_Posts = () => {
   const [data, setdata] = useState([]);
-  const [currentPosts, setcurrentPosts] = useState([]);
-  const [currentPage, setcurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(null);
-  const postsPerPage = 4;
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetchAllPosts();
       setdata(res.props.data);
-      const postData = res.props.data;
-      setTotalPages(Math.ceil(postData.length / 4));
-      setcurrentPosts(postData.slice(indexOfFirstPost, indexOfLastPost));
     };
     fetchData();
   }, []);
-  useEffect(() => {
-    setcurrentPosts(data.slice(indexOfFirstPost, indexOfLastPost));
-  }, [currentPage]);
   return (
     <div>
+      <style jsx>
+        {`
+          .swiper {
+            width: 100%;
+            padding-top: 50px;
+            padding-bottom: 50px;
+          }
+
+          .swiper-slide {
+            background-position: center;
+            background-size: cover;
+            width: 300px;
+            height: 300px;
+          }
+        `}
+      </style>
       <div className="flex items-center justify-center text-center py-8">
         <div className="flex flex-col p-2 m-2 ">
-          <div className="text-[3rem] text-center">
-            Featured Posts
-          </div>
+          <div className="text-[3rem] text-center">Featured Posts</div>
           <div className="text-[1rem] text-center my-4 mx-2">
             "Diving into the Code Ocean: Stories from a Hunter Coder's Desk"
           </div>
-          <div className="flex items-center align-middle flex-wrap justify-center md:flex-row lg:flex-row xl:flex-row flex-col">
-            {currentPosts
-              ? currentPosts.map((e, i) => {
-                  return (
-                    <Link href={`/blogs/${e.slug}`}>
-                      <div
-                        key={e.id}
-                        className="flex flex-col justify-center items-center"
-                      >
-                        {" "}
-                        <img
-                          src={
-                            e["_embedded"]["wp:featuredmedia"][0]["source_url"]
-                          }
-                          alt="No image"
-                          className="w-[80%]"
-                        />
-                        <div className="flex text-gray-500 text-sm m-2">
-                          <div className="m-1 font-bold">Vlog,</div>
-                          <div className="m-1">31 March, 2023</div>
-                        </div>
-                        <div className="font-bold text-black text-xl m-2">
-                          {e.title.rendered.length > 30
-                            ? e.title.rendered.substring(0, 30) + "..."
-                            : e.title.rendered}
-                        </div>
-                        <div
-                          className="text-sm text-gray-500 mt-4 m-2"
-                          dangerouslySetInnerHTML={{
-                            __html:
-                              e.excerpt.rendered.length > 65
-                                ? e.excerpt.rendered.substring(0, 65) + "..."
-                                : e.excerpt.rendered,
-                          }}
-                        ></div>
-                        <div className="flex cursor-pointer">
-                          <div className="m-2">
+          <div className="relative w-[100vw]">
+            <div id="app" className="w-[100vw]">
+              <Swiper
+                effect={"coverflow"}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={"auto"}
+                coverflowEffect={{
+                  rotate: 50,
+                  stretch: 0,
+                  depth: 100,
+                  modifier: 1,
+                  slideShadows: true,
+                }}
+                pagination={true}
+                modules={[EffectCoverflow, Pagination]}
+                className="mySwiper w-[100vw]"
+              >
+                {data &&
+                  data.map((e, i) => {
+                    return (
+                      <SwiperSlide className="w-[100vw]">
+                        <Link href={`/blogs/${e.slug}`}>
+                          <div
+                            key={e.id}
+                            className="flex flex-col"
+                          >
                             {" "}
                             <img
-                              src="https://source.unsplash.com/50x50/?girl"
-                              alt=""
-                              className=" rounded-full"
-                            />{" "}
-                          </div>
-                          <div className="grid m-1">
-                            <div className="font-bold text-sm hover:text-gray-600 mt-2">
-                              Max
+                              src={
+                                e["_embedded"]["wp:featuredmedia"][0][
+                                  "source_url"
+                                ]
+                              }
+                              alt="No image"
+                              className="w-[80%] md:w-full"
+                            />
+                            <div className="flex text-gray-500 text-sm m-2">
+                              <div className="m-1 font-bold">Vlog,</div>
+                              <div className="m-1">31 March, 2023</div>
                             </div>
-                            <div className=" text-sm hover:text-gray-600">
-                              Professor, California
+                            <div className="font-bold text-black text-xl m-2">
+                              {e.title.rendered.length > 30
+                                ? e.title.rendered.substring(0, 30) + "..."
+                                : e.title.rendered}
+                            </div>
+                            <div
+                              className="text-sm text-gray-500 mt-4 m-2"
+                              dangerouslySetInnerHTML={{
+                                __html:
+                                  e.excerpt.rendered.length > 65
+                                    ? e.excerpt.rendered.substring(0, 65) +
+                                      "..."
+                                    : e.excerpt.rendered,
+                              }}
+                            ></div>
+                            <div className="flex cursor-pointer">
+                              <div className="m-2">
+                                {" "}
+                                <img
+                                  src="https://source.unsplash.com/50x50/?girl"
+                                  alt=""
+                                  className=" rounded-full"
+                                />{" "}
+                              </div>
+                              <div className="grid m-1">
+                                <div className="font-bold text-sm hover:text-gray-600 mt-2">
+                                  Max
+                                </div>
+                                <div className=" text-sm hover:text-gray-600">
+                                  Professor, California
+                                </div>
+                              </div>
                             </div>
                           </div>
-                        </div>
-                      </div>
-                    </Link>
-                  );
-                })
-              : null}
+                        </Link>
+                      </SwiperSlide>
+                    );
+                  })}
+              </Swiper>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="w-full flex justify-center items-center">
-        <button
-          onClick={() => {
-            if (currentPage > 1) {
-              setcurrentPage(currentPage - 1);
-            }
-          }}
-          className={`bg-slate-200 ${
-            currentPage === 1 ? "hidden" : null
-          } ml-4 p-2 pl-4 pr-4 hover:bg-slate-500 hover:border-2 hover:border-solid hover:border-black transition-all duration-[0.5s] ease-in-out text-lg font-semibold text-black`}
-        >
-          Previous
-        </button>
-        <button
-          className={
-            "bg-slate-200 ml-4 p-2 pl-4 pr-4 hover:bg-slate-500 hover:border-2 hover:border-solid hover:border-black transition-all duration-[0.5s] ease-in-out text-lg font-semibold text-black"
-          }
-        >
-          {currentPage}
-        </button>
-        <button
-          onClick={() => {
-            if (data && currentPage <= totalPages) {
-              setcurrentPage(currentPage + 1);
-            }
-          }}
-          className={`bg-slate-200 ${
-            currentPage === totalPages ? "hidden" : null
-          } ml-4 p-2 pl-4 pr-4 hover:bg-slate-500 hover:border-2 hover:border-solid hover:border-black transition-all duration-[0.5s] ease-in-out text-lg font-semibold text-black`}
-        >
-          Next
-        </button>
       </div>
     </div>
   );
@@ -132,7 +135,7 @@ const Recently_Posts = () => {
 export async function fetchAllPosts(context) {
   // console.log(context)
   const res = await fetch(
-    `https://zthosting.com/wp-json/wp/v2/posts?_embed&per_page=50&_fields=title,date,slug,id,excerpt,_links&order=asc`
+    `https://zthosting.com/wp-json/wp/v2/posts?_embed&per_page=5&_fields=title,date,slug,id,excerpt,_links&order=asc`
   );
   const data = await res.json();
   //  console.log(data.length);
@@ -140,4 +143,3 @@ export async function fetchAllPosts(context) {
   return { props: { data } };
 }
 export default Recently_Posts;
-
