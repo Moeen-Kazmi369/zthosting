f2b9c0997c823c9364898e05efd36c99c05a84f1
@codewithkazmi <115881946+Moeen-Kazmi369@users.noreply.github.com>
2023-09-23 03:06:22 -0700

    jsbcysvw

--- src/components/GlobalStateProvider.js
+++ src/components/GlobalStateProvider.js
@@ -1,23 +1,23 @@
 "use client";
 // GlobalStateContext.js
 import { createContext, useContext, useState, useEffect } from "react";
-import Aos from "aos";
-import "aos/dist/aos.css";
+// import Aos from "aos";
+// import "aos/dist/aos.css";
 import React from "react";
 const GlobalStateContext = createContext();
 
 const GlobalStateProvider = ({ children }) => {
   const [theme, setTheme] = useState(false);
   const [LoginPage, setLoginPage] = useState(false);
-  useEffect(() => {
-    Aos.init({
-      duration: 1000,
-      offset: 200,
-      easing: "ease-in-sine",
-      // delay: 100,
-      // disable: window.innerWidth < 1024
-    });
-  }, []);
+  // useEffect(() => {
+  //   Aos.init({
+  //     duration: 1000,
+  //     offset: 200,
+  //     easing: "ease-in-sine",
+  //     // delay: 100,
+  //     // disable: window.innerWidth < 1024
+  //   });
+  // }, []);
   return (
     <GlobalStateContext.Provider
       value={{ setTheme, theme, LoginPage, setLoginPage }}
